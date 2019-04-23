e6bc85eb2eb2993c23a9a0435dbdfe510c048b05
German Viscuso <germanviscuso@gmail.com>
2019-04-24 00:50:40 +0200

    Added progressive code for intro APL workshop

---
+++ 06/lambda/index.js
@@ -0,0 +1,248 @@
+const Alexa = require('ask-sdk-core');
+
+// i18n dependency
+const i18n = require('i18next');
+const util = require('./util');
+
+// We create a language strings object containing all of our strings.
+// The keys for each string will then be referenced in our code
+// e.g. handlerInput.t('WELCOME_MSG')
+const languageStrings = {
+    en: {
+      translation: {
+        WELCOME_MSG: 'Welcome, you can say Hello or Help. Which would you like to try?',
+        HELLO_MSG: 'Hello Multimodal!',
+        HELP_MSG: 'You can say hello to me! How can I help?',
+        GOODBYE_MSG: 'Goodbye!',
+        REFLECTOR_MSG: 'You just triggered {{intent}}',
+        FALLBACK_MSG: 'Sorry, I don\'t know about that. Please try again.',
+        ERROR_MSG: 'Sorry, there was an error. Please try again.'
+      }
+    },
+    es:{
+      translation: {
+        WELCOME_MSG: 'Bienvenido, puedes decir Hola o Ayuda. Cual prefieres?',
+        HELLO_MSG: 'Hola Multimodal!',
+        HELP_MSG: 'Puedes decirme hola. Cómo te puedo ayudar?',
+        GOODBYE_MSG: 'Hasta luego!',
+        REFLECTOR_MSG: 'Acabas de activar {{intent}}',
+        FALLBACK_MSG: 'Lo siento, no se nada sobre eso. Por favor inténtalo otra vez.',
+        ERROR_MSG: 'Lo siento, ha habido un problema. Por favor inténtalo otra vez.'
+      }
+    }
+  }
+
+const LaunchRequestHandler = {
+    canHandle(handlerInput) {
+        return handlerInput.requestEnvelope.request.type === 'LaunchRequest';
+    },
+    handle(handlerInput) {
+        const speechText = handlerInput.t('WELCOME_MSG');
+
+        if(util.supportsAPL(handlerInput)){
+            handlerInput.responseBuilder.addDirective({
+                type: 'Alexa.Presentation.APL.RenderDocument',
+                version: '1.0',
+                token: 'SpeechDocumentToken',
+                document: require('./documents/template7.json'),
+                //datasources: require('./datasources/datasource4.json')
+                datasources: {
+                    templateData: {
+                        type: "object",
+                        properties: {
+                            header: "Hola Multimodal",
+                            background: "https://s3-eu-west-1.amazonaws.com/miscalexa/background.png",
+                            footer: "¿sabes rapear?",
+                            text: "¡Te sorprenderás!",
+                            textSsml: `<speak><say-as interpret-as="interjection">hola</say-as>. ¡Te sorprenderás!</speak>`,
+                            image: "https://s3-eu-west-1.amazonaws.com/miscalexa/Alexa-sticker_Logo_circle-logomark_1x1in.png",
+                            logo: "https://s3-eu-west-1.amazonaws.com/miscalexa/skilllogo.png"
+                        },
+                        transformers: [
+                            {
+                                inputPath: "footer",
+                                transformer: "textToHint"
+                            },
+                            {
+                                inputPath: "textSsml",
+                                outputName: "textSpeech",
+                                transformer: "ssmlToSpeech"
+                            }
+                        ]
+                    }
+                }
+              })
+              .addDirective({
+                type: 'Alexa.Presentation.APL.ExecuteCommands',
+                version: '1.0',
+                token: 'SpeechDocumentToken',
+                commands: [{
+                  "type": "SpeakItem",
+                  "componentId": "idVoiceDemoText",
+                  "highlightMode": "line"
+                }]
+          })
+        } else {
+            handlerInput.responseBuilder
+            .speak(speechText)
+            .reprompt(handlerInput.t('HELP_MSG'));
+        }
+
+        return handlerInput.responseBuilder.getResponse();
+    }
+};
+
+const HelloWorldIntentHandler = {
+    canHandle(handlerInput) {
+        return handlerInput.requestEnvelope.request.type === 'IntentRequest'
+            && handlerInput.requestEnvelope.request.intent.name === 'HelloWorldIntent';
+    },
+    handle(handlerInput) {
+        const speechText = handlerInput.t('HELLO_MSG');
+
+        return handlerInput.responseBuilder
+            .speak(speechText)
+            //.reprompt('add a reprompt if you want to keep the session open for the user to respond')
+            .getResponse();
+    }
+};
+
+const HelpIntentHandler = {
+    canHandle(handlerInput) {
+        return handlerInput.requestEnvelope.request.type === 'IntentRequest'
+            && handlerInput.requestEnvelope.request.intent.name === 'AMAZON.HelpIntent';
+    },
+    handle(handlerInput) {
+        const speechText = handlerInput.t('HELP_MSG');
+
+        return handlerInput.responseBuilder
+            .speak(speechText)
+            .reprompt(speechText)
+            .getResponse();
+    }
+};
+
+const CancelAndStopIntentHandler = {
+    canHandle(handlerInput) {
+        return handlerInput.requestEnvelope.request.type === 'IntentRequest'
+            && (handlerInput.requestEnvelope.request.intent.name === 'AMAZON.CancelIntent'
+                || handlerInput.requestEnvelope.request.intent.name === 'AMAZON.StopIntent');
+    },
+    handle(handlerInput) {
+        const speechText = handlerInput.t('GOODBYE_MSG');
+
+        return handlerInput.responseBuilder
+            .speak(speechText)
+            .getResponse();
+    }
+};
+
+const FallbackIntentHandler = {
+    canHandle(handlerInput) {
+        return handlerInput.requestEnvelope.request.type === 'IntentRequest'
+            && handlerInput.requestEnvelope.request.intent.name === 'AMAZON.FallbackIntent';
+    },
+    handle(handlerInput) {
+        const speechText = handlerInput.t('FALLBACK_MSG');
+
+        return handlerInput.responseBuilder
+            .speak(speechText)
+            .reprompt(handlerInput.t('HELP_MSG'))
+            .getResponse();
+    }
+};
+
+const SessionEndedRequestHandler = {
+    canHandle(handlerInput) {
+        return handlerInput.requestEnvelope.request.type === 'SessionEndedRequest';
+    },
+    handle(handlerInput) {
+        // Any cleanup logic goes here.
+        return handlerInput.responseBuilder.getResponse(); // notice we send an empty response
+    }
+};
+
+// The intent reflector is used for interaction model testing and debugging.
+// It will simply repeat the intent the user said. You can create custom handlers
+// for your intents by defining them above, then also adding them to the request
+// handler chain below.
+const IntentReflectorHandler = {
+    canHandle(handlerInput) {
+        return handlerInput.requestEnvelope.request.type === 'IntentRequest';
+    },
+    handle(handlerInput) {
+        const intentName = handlerInput.requestEnvelope.request.intent.name;
+        const speechText = handlerInput.t('REFLECTOR_MSG', {intent: intentName});
+
+        return handlerInput.responseBuilder
+            .speak(speechText)
+            //.reprompt('add a reprompt if you want to keep the session open for the user to respond')
+            .getResponse();
+    }
+};
+
+// Generic error handling to capture any syntax or routing errors. If you receive an error
+// stating the request handler chain is not found, you have not implemented a handler for
+// the intent being invoked or included it in the skill builder below.
+const ErrorHandler = {
+    canHandle() {
+        return true;
+    },
+    handle(handlerInput, error) {
+        const speechText = handlerInput.t('ERROR_MSG');
+
+        console.log(`~~~~ Error handled: ${error.message}`);
+
+        return handlerInput.responseBuilder
+            .speak(speechText)
+            .reprompt(handlerInput.t('HELP_MSG'))
+            .getResponse();
+    }
+};
+
+// This request interceptor will log all incoming requests to this lambda
+const LoggingRequestInterceptor = {
+    process(handlerInput) {
+        console.log(`Incoming request: ${JSON.stringify(handlerInput.requestEnvelope)}`);
+    }
+};
+
+// This response interceptor will log all outgoing responses of this lambda
+const LoggingResponseInterceptor = {
+    process(handlerInput, response) {
+        console.log(`Outgoing response: ${JSON.stringify(response)}`);
+    }
+};
+
+// This request interceptor will bind a translation function 't' to the handlerInput.
+const LocalisationRequestInterceptor = {
+    process(handlerInput) {
+        i18n.init({
+            lng: handlerInput.requestEnvelope.request.locale,
+            resources: languageStrings
+        }).then((t) => {
+            handlerInput.t = (...args) => t(...args);
+        });
+    }
+};
+
+// This handler acts as the entry point for your skill, routing all request and response
+// payloads to the handlers above. Make sure any new handlers or interceptors you've
+// defined are included below. The order matters - they're processed top to bottom.
+exports.handler = Alexa.SkillBuilders.custom()
+    .addRequestHandlers(
+        LaunchRequestHandler,
+        HelloWorldIntentHandler,
+        HelpIntentHandler,
+        CancelAndStopIntentHandler,
+        FallbackIntentHandler,
+        SessionEndedRequestHandler,
+        IntentReflectorHandler) // make sure IntentReflectorHandler is last so it doesn't override your custom intent handlers
+    .addErrorHandlers(
+        ErrorHandler)
+    .addRequestInterceptors(
+        LocalisationRequestInterceptor,
+        LoggingRequestInterceptor)
+    .addResponseInterceptors(
+        LoggingResponseInterceptor)
+    .lambda();
